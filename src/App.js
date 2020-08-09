bf0fbdbe345d30b950e48ccd7f807aca8a9a512b
BushraAyub <bushraayub44@gmail.com>
2020-08-09 20:03:33 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Button,Card,
+    CardBody,
+    CardImage,
+    CardTitle,
+    CardText,
+    CardSubtitle } from "reactstrap";
+
 
 function App() {
+  const [value , setValue]= useState ( [] );
+  const [name , setName]= useState (" ");
+  function changeState () {
+    let copyState = [...value];
+    copyState.push (name);
+    setValue (copyState);
+  }
+  console.log(name);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  
+    <input
+    name = "name"
+    value = {name}
+    onChange ={(e) => setName (e.target.value)}
+    />
+
+     <Button color="primary" size="lg" block onClick = { (e)=> changeState (e.target.value) }>
+     Press me to change state...
+     </Button>
+
+     {value.map((val) => { 
+       return  <h1> {val} </h1>
+     })}
+    </>
   );
 }
 
+    
 export default App;
